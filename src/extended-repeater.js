7d4d5e434ec88c658f0234d3ed1859f5cdc3166f
17ok <92027688+17ok@users.noreply.github.com>
2022-05-01 22:07:39 +0300

    feat: add two

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/*str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, { repeatTimes = 1,
+  separator = '+',
+  addition = '',
+  additionRepeatTimes = 1,
+  additionSeparator = '|'}) {
+ /*const { repeatTimes = 1,
+  separator = '+',
+  addition = '',
+  additionRepeatTimes = 1,
+  additionSeparator = '|'} = options;*/
+  
+  str = String(str);
+  addition = String(addition);
+  let additionString;
+  let x = additionSeparator.length;
+  addition === '' ? additionString = '' : additionString = (`${addition}${additionSeparator}`).repeat(additionRepeatTimes).slice(0, -x);
+  let subString = str + additionString;
+  let result = '';
+  let i = 0;
+  while (i < repeatTimes-1) {
+    result +=subString + separator;
+    i++
+  }
+  result +=subString;
+  return result;
+
+ // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
